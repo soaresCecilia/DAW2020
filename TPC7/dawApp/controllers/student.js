ccd76dbbfc6ae7bf2cf4b88f447a9659df4573f7
Cecilia Soares <ceciliasoares@MBP-de-Cecilia.home>
2020-11-28 23:44:32 +0000

    inicio app

---
+++ TPC7/dawApp/controllers/student.js
@@ -0,0 +1,42 @@
+// Student controller
+
+var Student = require('../models/student')
+
+// Returns student list
+module.exports.list = () => {
+    return Student
+        .find()
+        .sort({nome:1})
+        .exec()
+}
+
+module.exports.lookUp = id => {
+    return Student
+        .findOne({numero: id})
+        .exec()
+}
+
+module.exports.insert = student => {
+    var newStudent = new Student(student)
+    return newStudent.save()
+}
+
+module.exports.delete = id => {
+    return Student
+        .findByIdAndRemove({numero: id})
+        .exec()
+}
+
+
+module.exports.update = (id, campos) =>{
+    return Student
+        .findOneAndUpdate({numero: id}, {
+            $set: {
+                numero: campos['numero'],
+                nome: campos['nome'],
+                git: campos['git'],
+                tpc: campos['tpc']       
+            }
+        })
+        .exec()
+}
---
+++ TPC7/dawApp/routes/index.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var router = express.Router();
+
+var Student = require('../controllers/student')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/students', function(req, res) {
+  // Data retrieve
+  Student.list()
+    .then(data => res.render('students', { list: data }))
+    .catch(err => res.render('error', {error: err}))
+  ;
+});
+
+router.get('/:idStudent', function(req, res) {
+  Student.lookUp(req.params.idStudent)
+    .then(data => res.render('student', { Student: data }))
+    .catch(err => res.render('error', {error: err}))
+    ;
+});
+
+router.get('/students/registar', function(req, res) {
+  Student.list()
+    .then(data => res.render('students', { list: data }))
+    .catch(err => res.render('error', {error: err}))
+    ;
+});
+
+router.delete('/students/eliminar/:idStudent', function(req, res){
+  Student.delete(req.params.idStudent)
+    .then(data => res.render('ok', { ok: data }))
+    .catch(err => res.render('error', {error: err}))
+    ;
+});
+
+
+router.post('/students', function(req, res){
+  Student.insert(req.body)
+  .then(data => res.render('ok', { Student: data }))
+  .catch(err => res.render('error', {error: err}))
+  ;
+})
+
+router.put('/students/editar/:idStudent', function(req, res){
+  Student.update(req.params.idStudent, req.body)
+  .then(data => res.render('studentUpdate', { Student: data }))
+  .catch(err => res.render('error', {error: err}))
+  ;
+})
+
+
+module.exports = router;
